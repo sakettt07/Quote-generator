aeb98e718397bfbb0ff1443f1c5d2c1a62c66e9b
saket <saket@saketgupta.com>
2023-07-28 23:21:49 +0530

    New work

---
+++ script.js
@@ -0,0 +1,14 @@
+// console.log('this console.log is for checking that the the html is connected or not')
+
+// const quote = document.getElementById("quote");
+// const author = document.getElementById("author");
+// const api_url="https://api.quotable.io/random";
+
+// async function getquote(url){
+//     const response=await fetch(url);
+//     var data=await response.json();
+//     // console.log(data).
+//     quote.innerHTML=data.content;
+//     author.innerHTML=data.author;
+// }
+// getquote(api_url);
